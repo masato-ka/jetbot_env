350d1f945f4639de110f69df8af8bda50d65c2cc
masato-ka <jp6uzv@gmail.com>
2020-02-11 19:15:31 +0900

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-#TODO setup.py
 
+from setuptools import setup
 
+setup(
+    name="jetbot_env",
+    version="0.1.0",
+    install_requires=[],
+    extras_require={
+        "develop": []
+    },
+    entry_points={
+        "console_scripts": [
+        ],
+        "gui_scripts": [
+        ]
+    }
+)
